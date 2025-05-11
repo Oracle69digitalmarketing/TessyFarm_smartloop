0b47b5ab088ba905450a1501c14cbdc5b6833b30
Prince Adewale Adewumi <82568973+Oracle69digitalmarketing@users.noreply.github.com>
2025-05-11 13:54:29 +0100

    Update App.jsx

--- frontend_dashboard/src/App.jsx
+++ frontend_dashboard/src/App.jsx
@@ -2,9 +2,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import FarmsPage from './pages/FarmsPage';
+import FarmDetailPage from './pages/FarmDetailPage'; // <--- IMPORT NEW PAGE
 // Import other pages here later
-// import FarmDetailPage from './pages/FarmDetailPage'; 
-import './App.css'; // For global app styles
+// import FieldDetailPage from './pages/FieldDetailPage'; 
+import './App.css';
 
 function App() {
   return (
@@ -12,16 +13,18 @@ function App() {
       <div>
         <nav className="app-nav">
           <ul>
-            <li><Link to="/">Home (Farms)</Link></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/farms">Farms</Link></li>
             {/* Add other global links here */}
           </ul>
         </nav>
         <main className="app-content">
           <Routes>
-            <Route path="/" element={<FarmsPage />} />
+            <Route path="/" element={<FarmsPage />} /> {/* Default to FarmsPage */}
             <Route path="/farms" element={<FarmsPage />} /> 
-            {/* Example for a future farm detail page */}
-            {/* <Route path="/farms/:farmId" element={<FarmDetailPage />} /> */}
+            <Route path="/farms/:farmId" element={<FarmDetailPage />} /> {/* <--- ADDED ROUTE */}
+            {/* Example for a future field detail page */}
+            {/* <Route path="/fields/:fieldId" element={<FieldDetailPage />} /> */}
             {/* Add more routes here */}
           </Routes>
         </main>
