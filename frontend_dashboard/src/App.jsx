f564c0d5596ca26f068f240d6f4bafecb5417246
Prince Adewale Adewumi <82568973+Oracle69digitalmarketing@users.noreply.github.com>
2025-05-11 17:52:24 +0100

    Update App.jsx

--- frontend_dashboard/src/App.jsx
+++ frontend_dashboard/src/App.jsx
@@ -1,12 +1,11 @@
 // frontend_dashboard/src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import FarmsPage from './pages/FarmsPage';
-import FarmDetailPage from './pages/FarmDetailPage';
-import FieldDetailPage from './pages/FieldDetailPage';
-import CropCycleDetailPage from './pages/CropCycleDetailPage';
-import AddFarmPage from './pages/AddFarmPage'; // <--- IMPORT
-import EditFarmPage from './pages/EditFarmPage'; // <--- IMPORT
+// ... (other page imports)
+import AddFarmPage from './pages/AddFarmPage';
+import EditFarmPage from './pages/EditFarmPage';
+import AddFieldPage from './pages/AddFieldPage'; // <--- IMPORT
+import EditFieldPage from './pages/EditFieldPage'; // <--- IMPORT
 import './App.css';
 
 function App() {
@@ -17,17 +16,24 @@ function App() {
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/farms">Farms</Link></li>
-            <li><Link to="/farms/new">Add Farm</Link></li> {/* <--- ADD LINK */}
+            <li><Link to="/farms/new">Add Farm</Link></li>
+            {/* General "Add Field" link, farm selected in form */}
+            <li><Link to="/fields/new">Add Field</Link></li> 
           </ul>
         </nav>
         <main className="app-content">
           <Routes>
-            <Route path="/" element={<FarmsPage />} />
-            <Route path="/farms" element={<FarmsPage />} /> 
-            <Route path="/farms/new" element={<AddFarmPage />} /> {/* <--- ADD ROUTE */}
-            <Route path="/farms/:farmId" element={<FarmDetailPage />} />
-            <Route path="/farms/:farmId/edit" element={<EditFarmPage />} /> {/* <--- ADD ROUTE */}
+            {/* ... (Farm routes) ... */}
+            <Route path="/farms/new" element={<AddFarmPage />} />
+            <Route path="/farms/:farmId/edit" element={<EditFarmPage />} />
+            
+            {/* Field Routes */}
+            <Route path="/fields/new" element={<AddFieldPage />} /> {/* General add field */}
+            <Route path="/farms/:farmId/fields/new" element={<AddFieldPage />} /> {/* Add field under specific farm */}
             <Route path="/fields/:fieldId" element={<FieldDetailPage />} />
+            <Route path="/fields/:fieldId/edit" element={<EditFieldPage />} />
+            
+            {/* ... (Crop Cycle routes) ... */}
             <Route path="/crop-cycles/:cycleId" element={<CropCycleDetailPage />} />
           </Routes>
         </main>
@@ -37,7 +43,3 @@ function App() {
 }
 
 export default App;
-
-// In frontend_dashboard/src/services/api.js
-export const getFarmById = (farmId) => apiClient.get(`/farms/${farmId}`);
-export const updateFarm = (farmId, farmData) => apiClient.put(`/farms/${farmId}`, farmData);
